cc450b08d817e42fe7f67a8eafed7744c2e5d359
rruuppaamm <rupamkumar3641@gmail.com>
2026-08-18 10:40:41 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String rupam[]) {
+        System.out.println("Hello, World");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        sc.close();
+    }
+
+} 
